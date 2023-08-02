9f1e68366f55157a5079396e97f872118e5ca87c
p.severin <p.severin@uds.systems>
2023-08-02 16:50:20 +0400

    lesson 13

--- lesson-12.ts
+++ lesson-12.ts
@@ -1,11 +1,11 @@
-const test: null = null;
-const test2: any = null;
-const test3: string = null;
-const test4: number = null;
+// const test: null = null;
+// const test2: any = null;
+// const test3: string = null;
+// const test4: number = null;
 
-const test5: undefined = undefined;
-const test6: any = undefined;
-const test7: string = undefined;
+// const test5: undefined = undefined;
+// const test6: any = undefined;
+// const test7: string = undefined;
 
 let smth; // any type
 
---
+++ lesson-13.ts
@@ -0,0 +1,12 @@
+let id: symbol = Symbol('id');
+
+const data13 = {
+  [id]: 1
+};
+
+console.log(data13[id]);
+
+const num1: bigint = 1n;
+const num2: bigint = 2n;
+
+console.log(num1 + num2);
